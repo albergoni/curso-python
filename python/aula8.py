4425efe1a536680073bfa02e2fd312151c16c4db
albergoni <albergoni@gmail.com>
2023-05-08 15:10:21 -0300

    manipulacao de variaveis

---
+++ python/aula8.py
@@ -0,0 +1,15 @@
+#declaração de variáveis
+nome = "Davi"
+sobrenome = "Souza Antunes"
+idade  = 43
+ano_nascimento = 1980
+maior_de_idade = idade >= 18
+altura_metros = 1.86
+
+#programa
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade?',maior_de_idade)
+print('Altura em metros:',altura_metros)

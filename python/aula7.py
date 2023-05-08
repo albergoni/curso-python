908821d092e9a6f4f25139a13a63f4056acbfa37
albergoni <albergoni@gmail.com>
2023-05-08 15:09:48 -0300

    programa variaveis

--- python/aula7.py
+++ python/aula7.py
@@ -12,7 +12,7 @@
 # print(nome_completo, soma_dois_mais_dois)
 
 nome = 'Luiz'
-idade = 17
+idade = 18
 maior_de_idade = idade >= 18
 print('Nome:', nome, 'Idade:', idade)
 print('É maior?', maior_de_idade)
